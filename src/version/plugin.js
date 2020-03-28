afc6e62e922efdfdb146e8d60dc8d4e008e7eea4
Attila Bagossy <bagossyattila@outlook.com>
2020-03-28 20:49:30 +0100

    Retrieving, logging and returning the application version

--- src/version/plugin.js
+++ src/version/plugin.js
@@ -1,8 +1,17 @@
+const config = require('../config/config');
+const log = require('../util/log');
+const versionConfig = require('./config/config');
+
+
 const routePaths = [
     './web/getVersion.js'
 ];
 
 async function versionPlugin(fastify) {
+    versionConfig.configureVersion(config);
+
+    log.info(`Deployed application version is ${config.get('version.pretty')}`);
+
     routePaths
         .map(require)
         .forEach(route => fastify.route(route));    
--- src/version/web/getVersion.js
+++ src/version/web/getVersion.js
@@ -1,7 +1,10 @@
+const config = require('../../config/config').get('version');
+
+
 module.exports = {
     method: 'GET',
     url: '/version',
     async handler() {
-        return 'empty';
+        return config.pretty;
     }
 };
