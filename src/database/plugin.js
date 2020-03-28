f7e9daea6ee65e836659528bb93db48cf9fe78e4
Attila Bagossy <bagossyattila@outlook.com>
2020-03-28 18:18:51 +0100

    Actually calling the migration check

--- src/database/plugin.js
+++ src/database/plugin.js
@@ -1,4 +1,5 @@
 const database = require('./database');
+const migration = require('./migration/migration');
 const shutdown = require('../shutdown/shutdown');
 
 
@@ -6,6 +7,8 @@ async function databasePlugin() {
     await database.open();
 
     shutdown.registerHook(async () => await database.close());
+
+    await migration.checkDatabaseVersion();
 };
 
 module.exports = databasePlugin;
