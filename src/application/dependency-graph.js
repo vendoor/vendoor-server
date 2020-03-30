d6b01f9fa578bb9ce7a713edfb5bea040963bc75
Attila Bagossy <bagossyattila@outlook.com>
2020-03-30 20:43:21 +0200

    Refactored the dependency resolution

--- src/application/dependency-graph.js
+++ src/application/dependency-graph.js
@@ -1,29 +1,53 @@
-function resolve (allComponents) {
-  const orderedComponents = [];
+function resolve (componentRegistry) {
+  const orderedComponents = []
 
-  (function innerResolve (components) {
-    for (const component of components) {
-      if (!Array.isArray(component.dependencies) || component.dependencies.length === 0) {
-        if (!orderedComponents.find(c => c.name === component.name)) {
-          orderedComponents.push(component)
-        }
-      } else {
-        const dependencyObjects = component.dependencies.map(name => {
-          const c = allComponents[name]
+  for (const componentName of Object.keys(componentRegistry)) {
+    innerResolve([componentName], componentRegistry[componentName].dependencies)
 
-          if (!c) {
-            throw new Error(`Unknown component detected "${name}" (dependency of ${component.name})`)
-          }
-
-          return c
-        })
-
-        innerResolve(dependencyObjects)
-      }
-    }
-  })(Object.values(allComponents))
+    pushComponent(orderedComponents, componentRegistry[componentName])
+  }
 
   return orderedComponents
+
+  function innerResolve (resolutionStack, dependencyNamesOfCurrentComponent) {
+    for (const dependencyName of dependencyNamesOfCurrentComponent) {
+      guardCyclicDependencies(resolutionStack, dependencyName)
+
+      const dependency = componentRegistry[dependencyName]
+
+      guardUnknownDependency(resolutionStack, dependencyName, dependency)
+
+      if (Array.isArray(dependency.dependencies)) {
+        resolutionStack.push(dependencyName)
+
+        innerResolve(resolutionStack, dependency.dependencies)
+      }
+
+      pushComponent(orderedComponents, dependency)
+    }
+  }
+}
+
+function guardCyclicDependencies (resolutionStack, dependencyName) {
+  if (resolutionStack.includes(dependencyName)) {
+    throw new Error(`Cyclic dependencies detected: ${resolutionStack.join(' -> ')} -> ${dependencyName}`)
+  }
+}
+
+function guardUnknownDependency (resolutionStack, dependencyName, dependency) {
+  const parent = resolutionStack.length === 0
+    ? '* top level which should not happen'
+    : resolutionStack[resolutionStack.length - 1]
+
+  if (!dependency) {
+    throw new Error(`Unknown component detected "${dependencyName}" (dependency of ${parent})`)
+  }
+}
+
+function pushComponent (orderedComponents, component) {
+  if (!orderedComponents.find(c => c.name === component.name)) {
+    orderedComponents.push(component)
+  }
 }
 
 module.exports = {
