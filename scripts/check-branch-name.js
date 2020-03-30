7beab57944c53dfd6e05741f2f32deb9ed7b28c8
Attila Bagossy <bagossyattila@outlook.com>
2020-03-30 15:22:24 +0200

    StandardJS lint fixes (sorry)

--- scripts/check-branch-name.js
+++ scripts/check-branch-name.js
@@ -2,10 +2,10 @@ const { execSync } = require('child_process')
 
 const branch = require('./lib/branch')
 
-const CURRENT_BRANCH_COMMAND = 'git rev-parse --abbrev-ref HEAD';
+const CURRENT_BRANCH_COMMAND = 'git rev-parse --abbrev-ref HEAD'
 
 const OWNER = 'vendoor'
-const REPOSITORY = 'vendoor-server'
+const REPOSITORY = 'vendoor-server';
 
 (async function main () {
   const currentBranch = execSync(CURRENT_BRANCH_COMMAND, { encoding: 'utf-8' }).trim()
--- scripts/generate-branch-name.js
+++ scripts/generate-branch-name.js
@@ -1,20 +1,20 @@
 const { execSync } = require('child_process')
 
-const branch = require('./lib/branch');
+const branch = require('./lib/branch')
 
 const OWNER = 'vendoor'
-const REPOSITORY = 'vendoor-server'
+const REPOSITORY = 'vendoor-server';
 
-(async function main() {
-    let issueNumber
-    try {
-        issueNumber = Number.parseInt(process.argv[2])
-    } catch {
-        console.log(`The issue number provided "${issueNumber}" is not a valid number!\n\n  Usage: npm generate:branch -- 17`)
-        process.exit(1)
-    }
+(async function main () {
+  let issueNumber
+  try {
+    issueNumber = Number.parseInt(process.argv[2])
+  } catch {
+    console.log(`The issue number provided "${issueNumber}" is not a valid number!\n\n  Usage: npm generate:branch -- 17`)
+    process.exit(1)
+  }
 
-    const branchName = await branch.branchNameForIssue(OWNER, REPOSITORY, issueNumber)
+  const branchName = await branch.branchNameForIssue(OWNER, REPOSITORY, issueNumber)
 
-    execSync(`git checkout -b ${branchName}`)
+  execSync(`git checkout -b ${branchName}`)
 })()
