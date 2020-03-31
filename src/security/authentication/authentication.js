27938beb77c3c52c6dce44b3912404ee898433d2
Attila Bagossy <bagossyattila@outlook.com>
2020-03-31 16:08:57 +0200

    Created a stub for the authentication module

---
+++ src/security/authentication/authentication.js
@@ -0,0 +1,12 @@
+async function validateToken(token) {
+    throw new Error('@viktorsimko Please implement me.')
+}
+
+async function extractDataFromToken(token) {
+    throw new Error('@viktorsimko Please implement me.')
+}
+
+module.exports = {
+    validateToken,
+    extractDataFromToken
+}
