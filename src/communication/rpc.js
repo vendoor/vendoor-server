009e6c39906f4832b9fb3a9e9c2f7c04fe38878c
Attila Bagossy <bagossyattila@outlook.com>
2020-04-01 20:58:08 +0200

    communication.rpc.registerRpcHandler now accepts an object

--- src/communication/rpc.js
+++ src/communication/rpc.js
@@ -1,7 +1,7 @@
 const impl = require('./comlink/rpc')
 
 module.exports = {
-  registerRpcHandler (path, func) {
-    impl.instance().registerRpcHandler(path, func)
+  registerRpcHandler ({ path, handler }) {
+    impl.instance().registerRpcHandler(path, handler)
   }
 }
