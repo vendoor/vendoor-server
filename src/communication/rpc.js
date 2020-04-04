5b535c32d8091180c27978d3326f4c2604e3b9da
Attila Bagossy <bagossyattila@outlook.com>
2020-04-04 14:39:17 +0200

    Documentation for rpc.registerRpcHandler

--- src/communication/rpc.js
+++ src/communication/rpc.js
@@ -1,7 +1,23 @@
 const impl = require('./comlink/rpc')
 
 module.exports = {
-  registerRpcHandler ({ path, handler }) {
+  /**
+   * Registers a new RPC handler.
+   * @param {Object} handlerRegistration Information about the handler being registered.
+   * @param {string} handlerRegistration.path The path to attach the handler on. Paths should use the dot notation for namespacing:
+   *                                          location.getLocationsForUser
+   * @param {function} handlerRegistration.handler The actual handler function. Async function are allowed.
+   * @param {Object} [handlerRegistration.schema] JSON schemas for request and response description and validation.
+   * @param {Object} [handlerRegistration.schema.request] JSON schema for request description and validation.
+   *                                                      Requests violating this schema will be discarded.
+   * @param {Object} [handlerRegistration.schema.response] JSON schema for response description and validation.
+   *                                                       Responses violating this schema will be discarded.
+   * @param {Object} [handlerRegistration.meta] Metainformation about the handler being registered
+   * @param {string} [handlerRegistration.meta.title] The title of the handler.
+   * @param {string} [handlerRegistration.meta.description] Markdown formatted description of the handler.
+   * @param {string[]} [handlerRegistration.meta.tags] Arbitrary string tags.
+   */
+  registerRpcHandler (handlerRegistration) {
     impl.instance().registerRpcHandler(path, handler)
   }
 }
